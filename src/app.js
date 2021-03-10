72fb421ddc1b4dd1fc0030195bf57808f73f0fae
clb-alper <52celebialper@gmail.com>
2021-03-10 03:32:58 +0300

    add about text

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3003
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
