238b6d57734660e0c1c9b7d1f8056568e4ec9d78
clb-alper <52celebialper@gmail.com>
2021-03-10 03:22:50 +0300

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+
 const app = express();
+const port = process.env.PORT || 3002
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -99,6 +101,6 @@ app.get('*', (req,res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port '+ port)
 })
