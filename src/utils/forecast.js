adea755d6c80607afa3499efba1802cb2aa14447
clb-alper <52celebialper@gmail.com>
2021-03-10 03:37:00 +0300

    add humidity text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,4 @@
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 
 const request = require("postman-request");
 
@@ -21,7 +12,7 @@ const forecast = (lt, lg, callback) => {
         } else if (body.error) {
             callback('Unable to find a location. Please try another search', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+ '. Its currently ' + body.current.temperature + ' degree out!' )
+            callback(undefined, body.current.weather_descriptions[0]+ '. Its currently ' + body.current.temperature + ' degree out! +' +'Humidity:'+ body.current.humidity +' )
         }
     })
 
